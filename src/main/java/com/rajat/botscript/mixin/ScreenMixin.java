fa0cf921d58a56595c58b8b216117984bc337514
RajatBG1 <rajatsingla712@gmail.com>
2025-03-22 10:24:10 +0530

    2

--- src/main/java/com/rajat/botscript/mixin/ScreenMixin.java
+++ src/main/java/com/rajat/botscript/mixin/ScreenMixin.java
@@ -1,14 +1,9 @@
-package com.rajat.botscript.mixin;
-
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
-import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.text.Text;
 import net.minecraft.client.gui.screen.option.OptionsScreen;
-import net.minecraft.client.gui.screen.option.GameOptionsScreen;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -28,7 +23,8 @@ public abstract class ScreenMixin extends Screen {
                 button -> {
                     // Open your script GUI when clicked
                     this.client.setScreen(new ScriptGui(this));
-                }
+                },
+                new CustomNarrationSupplier()
         ));
     }
 }
